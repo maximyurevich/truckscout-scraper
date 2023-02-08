86ecc00097f1e7ba4b1b89c8800417b4d427a55f
Maxim Yurevich <maxim.yurewitch@gmail.com>
2023-02-09 00:16:59 +0300

    feat: add data folder and data.json creation code

--- scraper.py
+++ scraper.py
@@ -1,28 +1,17 @@
 """Scraper for truckscout24."""
 import asyncio
 import re
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from io import BytesIO
 from typing import Optional
 import json
+import os
 
 import httpx
-import pyppeteer
 from bs4 import BeautifulSoup
 from PIL import Image
 
-
-@dataclass
-class Ad:
-    id: int
-    href: str
-    title: str
-    price: int
-    mileage:int
-    color: str
-    power: int
-    description: str
-
+from playwright.sync_api import sync_playwright
 
 urls = []
 page_count = 4
@@ -41,7 +30,9 @@ def get_html(url: str):
 
 
 def download_image(url: str):
-    pass
+    response = httpx.get(url)
+    img = Image.open(BytesIO(response.content))
+    return img
 
 
 def get_ads_links():
@@ -49,9 +40,7 @@ def get_ads_links():
 
     for i in range(page_count):
         links.append(
-            BeautifulSoup(get_html(urls[i]), "html.parser").select_one(".ls-titles a")[
-                "href"
-            ]
+            BeautifulSoup(get_html(urls[i]), "lxml").select_one(".ls-titles a")["href"]
         )
 
     return links
@@ -62,37 +51,52 @@ def get_ads_data():
     url: str = "https://www.truckscout24.de"
     urls = [url + link for link in get_ads_links()]
 
-    for i in range(page_count):
-        page_html = get_html(urls[i])
-        soup = BeautifulSoup(page_html, "html.parser")
+    with sync_playwright() as p:
+        for i in range(page_count):
+            page_html = get_html(urls[i])
+            soup = BeautifulSoup(page_html, "lxml")
+            columns_soup = soup.select_one(".columns")
 
-        columns_soup = soup.select_one(".columns")
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            page.goto(urls[i])
 
-        ads.append(
-            Ad(
-                id=i + 1,
-                href=urls[i],
-                title=soup.select_one(".sc-ellipsis").get_text(),
-                price=re.search(
-                    r"€ (\d+\.?\d*)", soup.select_one(".d-price > h2").get_text()
-                ).group(1),
-                mileage=re.search(
-                    r"(\d+\.?\d*) km", soup.select_one(".data-basic1").get_text()
-                ).group(1),
-                color=columns_soup.select_one("li:nth-child(n+9)")
-                .select_one("div:nth-child(n+2)")
-                .get_text(),
-                power=re.search(
-                    r"(\d+\.?\d*) kW", 
-                    columns_soup
-                        .select_one("li:nth-child(n+11)")
+            # For description
+
+            browser.close()
+
+            ads.append(
+                dict(
+                    id=i + 1,
+                    href=urls[i],
+                    title=soup.select_one(".sc-ellipsis").get_text(),
+                    price=re.search(
+                        r"€ (\d+\.?\d*)", soup.select_one(".d-price > h2").get_text()
+                    ).group(1),
+                    mileage=re.search(
+                        r"(\d+\.?\d*) km", soup.select_one(".data-basic1").get_text()
+                    ).group(1),
+                    color=columns_soup.select_one("li:nth-child(n+9)")
+                    .select_one("div:nth-child(n+2)")
+                    .get_text(),
+                    power=re.search(
+                        r"(\d+\.?\d*) kW",
+                        columns_soup.select_one("li:nth-child(n+11)")
                         .select_one("div:nth-child(n+2)")
-                        .get_text()
-                ).group(1),
-                description="",
+                        .get_text(),
+                    ).group(1),
+                    description="",
+                )
             )
-        )
     return ads
 
 
-print(get_ads_data())
+def main():
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    with open("data/data.json", "w") as data:
+        data.write(json.dumps({}, indent=4))
+
+
+if __name__ == "__main__":
+    main()
