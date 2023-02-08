a7d137ce2afa2135fb593fbd291ae8bd5e39978b
Maxim Yurevich <maxim.yurewitch@gmail.com>
2023-02-08 18:47:12 +0300

    feat: add pages scraping function

--- scraper.py
+++ scraper.py
@@ -3,6 +3,10 @@ from dataclasses import dataclass
 
 import httpx
 from bs4 import BeautifulSoup
+from typing import Optional
+from io import BytesIO
+from PIL import Image
+import re
 
 
 @dataclass
@@ -10,17 +14,17 @@ class Ad:
     id: int
     href: str
     title: str
-    price: int
-    mileage: int
-    color: str
-    power: int
+    price: int | str
+    mileage: Optional[int | str]
+    color: Optional[str]
+    power: Optional[int]
     description: str
 
 
 urls = []
 page_count = 4
 
-for i in range(0, page_count):
+for i in range(page_count):
     urls.append(
         f"https://www.truckscout24.de"
         f"/transporter/gebraucht/kuehl-iso-frischdienst/renault"
@@ -33,20 +37,45 @@ def get_html(url: str):
     return response
 
 
+def download_image(url: str):
+    pass
+
+
 def get_ads_links():
     links = []
 
-    for i in range(0, page_count):
+    for i in range(page_count):
         links.append(
-            BeautifulSoup(get_html(urls[i]), "html.parser").select_one(
-                ".ls-titles a"
-            )["href"]
+            BeautifulSoup(get_html(urls[i]), "html.parser").select_one(".ls-titles a")[
+                "href"
+            ]
         )
 
     return links
 
-def get_ad_data():
-    pass
+
+def get_ads_data():
+    ads = []
+    url: str = "https://www.truckscout24.de"
+    urls = [url + link for link in get_ads_links()]
+
+    for i in range(page_count):
+        page_html = get_html(urls[i])
+        soup = BeautifulSoup(page_html, "html.parser")
+
+        ads.append(
+            Ad(
+                id=i + 1,
+                href=urls[i],
+                title=soup.select_one(".sc-ellipsis").get_text(),
+                price=soup.select_one(".d-price > h2").get_text(),
+                mileage="",
+                color="",
+                power=0,
+                description="",
+            )
+        )
+    return ads
 
 
-print(get_ads_links())
+print(get_ads_data())
